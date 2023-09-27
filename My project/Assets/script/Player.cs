6b890427ca266bb952ebe366d20780a3421ae08f
Giovanna Gabriela Dantas Soares <20221170150028@ifrn.local>
2023-09-27 10:02:40 -0300

    nada

---
+++ My project/Assets/script/Player.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int velocidade = 10;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TryGetComponent(out rb); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 direcao = new Vector3(h,0,v);
+        rb.AddForce(direcao * velocidade * Time.deltaTime);
+    }
+}
